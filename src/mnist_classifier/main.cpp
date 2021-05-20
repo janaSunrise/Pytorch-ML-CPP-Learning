90e3d9fd03892d21dc9dc64276e112e9eec0c319
janaSunrise <warriordefenderz@gmail.com>
2021-05-20 16:59:38 +0530

    Improve MNIST.

--- src/mnist_classifier/main.cpp
+++ src/mnist_classifier/main.cpp
@@ -1,67 +1,163 @@
 #include <torch/torch.h>
 
+#include <cstddef>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Where to find the MNIST dataset.
+const char* kDataRoot = "../../../data/mnist";
+
+// The batch size for training.
+const int64_t kTrainBatchSize = 64;
+
+// The batch size for testing.
+const int64_t kTestBatchSize = 1000;
+
+// The number of epochs to train.
+const int64_t kNumberOfEpochs = 10;
+
+// After how many batches to log a new update with the loss value.
+const int64_t kLogInterval = 10;
+
 struct Net : torch::nn::Module {
-    Net() {
-        // Construct and register two Linear submodules.
-        fc1 = register_module("fc1", torch::nn::Linear(784, 64));
-        fc2 = register_module("fc2", torch::nn::Linear(64, 32));
-        fc3 = register_module("fc3", torch::nn::Linear(32, 10));
+    Net() :
+        conv1(torch::nn::Conv2dOptions(1, 10, /*kernel_size=*/5)),
+        conv2(torch::nn::Conv2dOptions(10, 20, /*kernel_size=*/5)),
+        fc1(320, 50),
+        fc2(50, 10)
+    {
+        register_module("conv1", conv1);
+        register_module("conv2", conv2);
+        register_module("conv2_drop", conv2_drop);
+        register_module("fc1", fc1);
+        register_module("fc2", fc2);
     }
 
-    // Implement the Net's algorithm.
     torch::Tensor forward(torch::Tensor x) {
-        x = torch::relu(fc1->forward(x.reshape({x.size(0), 784})));
-        x = torch::dropout(x, /*p=*/0.5, /*train=*/is_training());
-        x = torch::relu(fc2->forward(x));
-        x = torch::log_softmax(fc3->forward(x), /*dim=*/1);
-
-        return x;
+        x = torch::relu(torch::max_pool2d(conv1->forward(x), 2));
+        x = torch::relu(
+            torch::max_pool2d(conv2_drop->forward(conv2->forward(x)), 2)
+        );
+        x = x.view({-1, 320});
+        x = torch::relu(fc1->forward(x));
+        x = torch::dropout(x, /*p=*/0.5, /*training=*/is_training());
+        x = fc2->forward(x);
+        return torch::log_softmax(x, /*dim=*/1);
     }
 
-    torch::nn::Linear fc1{nullptr}, fc2{nullptr}, fc3{nullptr};
+    torch::nn::Conv2d conv1;
+    torch::nn::Conv2d conv2;
+    torch::nn::Dropout2d conv2_drop;
+    torch::nn::Linear fc1;
+    torch::nn::Linear fc2;
 };
 
-int main() {
-    // Create a new Net.
-    auto net = std::make_shared<Net>();
+template <typename DataLoader>
+void train(
+    size_t epoch,
+    Net& model,
+    torch::Device device,
+    DataLoader& data_loader,
+    torch::optim::Optimizer& optimizer,
+    size_t dataset_size
+) {
+    model.train();
+    size_t batch_idx = 0;
+    for (auto& batch : data_loader) {
+        auto data = batch.data.to(device), targets = batch.target.to(device);
+        optimizer.zero_grad();
 
-    // Create a multi-threaded data loader for the MNIST dataset.
-    auto data_loader = torch::data::make_data_loader(
-        torch::data::datasets::MNIST("../../../data/mnist").map(torch::data::transforms::Stack<>()),
-        /*batch_size=*/64
+        auto output = model.forward(data);
+        auto loss = torch::nll_loss(output, targets);
+
+        AT_ASSERT(!std::isnan(loss.template item<float>()));
+
+        loss.backward();
+        optimizer.step();
+
+        if (batch_idx++ % kLogInterval == 0) {
+        std::printf(
+            "\rTrain Epoch: %ld [%5ld/%5ld] Loss: %.4f",
+            epoch,
+            batch_idx * batch.data.size(0),
+            dataset_size,
+            loss.template item<float>());
+        }
+    }
+}
+
+template <typename DataLoader>
+void test(
+    Net& model,
+    torch::Device device,
+    DataLoader& data_loader,
+    size_t dataset_size
+) {
+    torch::NoGradGuard no_grad;
+    model.eval();
+    double test_loss = 0;
+    int32_t correct = 0;
+    for (const auto& batch : data_loader) {
+        auto data = batch.data.to(device), targets = batch.target.to(device);
+        auto output = model.forward(data);
+        test_loss += torch::nll_loss(
+                        output,
+                        targets,
+                        /*weight=*/{},
+                        torch::Reduction::Sum
+                     ).template item<float>();
+
+        auto pred = output.argmax(1);
+        correct += pred.eq(targets).sum().template item<int64_t>();
+    }
+
+    test_loss /= dataset_size;
+    std::printf(
+        "\nTest set: Average loss: %.4f | Accuracy: %.3f\n",
+        test_loss,
+        static_cast<double>(correct) / dataset_size
+    );
+}
+
+auto main() -> int {
+    torch::manual_seed(1);
+
+    torch::DeviceType device_type;
+    if (torch::cuda::is_available()) {
+        std::cout << "CUDA available! Training on GPU." << std::endl;
+        device_type = torch::kCUDA;
+    } else {
+        std::cout << "Training on CPU." << std::endl;
+        device_type = torch::kCPU;
+    }
+    torch::Device device(device_type);
+
+    Net model;
+    model.to(device);
+
+    auto train_dataset = torch::data::datasets::MNIST(kDataRoot)
+                         .map(torch::data::transforms::Normalize<>(0.1307, 0.3081))
+                         .map(torch::data::transforms::Stack<>());
+    const size_t train_dataset_size = train_dataset.size().value();
+    auto train_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
+        std::move(train_dataset), kTrainBatchSize
     );
 
-    // Instantiate an SGD optimization algorithm to update our Net's parameters.
-    torch::optim::SGD optimizer(net->parameters(), /*lr=*/0.01);
+    auto test_dataset = torch::data::datasets::MNIST(kDataRoot, torch::data::datasets::MNIST::Mode::kTest)
+                        .map(torch::data::transforms::Normalize<>(0.1307, 0.3081))
+                        .map(torch::data::transforms::Stack<>());
 
-    for (size_t epoch = 1; epoch <= 10; ++epoch) {
-        size_t batch_index = 0;
+    const size_t test_dataset_size = test_dataset.size().value();
+    auto test_loader =torch::data::make_data_loader(std::move(test_dataset), kTestBatchSize);
 
-        // Iterate the data loader to yield batches from the dataset.
-        for (auto& batch : *data_loader) {
-            // Reset gradients.
-            optimizer.zero_grad();
+    torch::optim::SGD optimizer(
+        model.parameters(), torch::optim::SGDOptions(0.01).momentum(0.5)
+    );
 
-            // Execute the model on the input data.
-            torch::Tensor prediction = net->forward(batch.data);
-
-            // Compute a loss value to judge the prediction of our model.
-            torch::Tensor loss = torch::nll_loss(prediction, batch.target);
-
-            // Compute gradients of the loss w.r.t. the parameters of our model.
-            loss.backward();
-
-            // Update the parameters based on the calculated gradients.
-            optimizer.step();
-
-            // Output the loss and checkpoint every 100 batches.
-            if (++batch_index % 100 == 0) {
-                std::cout << "Epoch: " << epoch << " | Batch: " << batch_index
-                          << " | Loss: " << loss.item<float>() << std::endl;
-
-                // Serialize your model periodically as a checkpoint.
-                torch::save(net, "net.pt");
-            }
-        }
+    for (size_t epoch = 1; epoch <= kNumberOfEpochs; ++epoch) {
+        train(epoch, model, device, *train_loader, optimizer, train_dataset_size);
+        test(model, device, *test_loader, test_dataset_size);
     }
 }
