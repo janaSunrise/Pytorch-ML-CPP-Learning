6676d917f88949ce2abc3762b5d92e3b8af4afdb
janaSunrise <warriordefenderz@gmail.com>
2021-05-21 15:06:42 +0530

    Added Custom autograd function too.

--- src/autograd/main.cpp
+++ src/autograd/main.cpp
@@ -103,6 +103,81 @@ void compute_higher_order_gradients() {
     std::cout << input.grad() << std::endl;
 }
 
+class LinearFunction : public Function<LinearFunction> {
+    public:
+    // Both forward and backward are static functions
+    static torch::Tensor forward(
+        AutogradContext *ctx, torch::Tensor input, torch::Tensor weight, torch::Tensor bias = torch::Tensor()
+    ) {
+        ctx->save_for_backward({input, weight, bias});
+        auto output = input.mm(weight.t());
+
+        if (bias.defined()) {
+            output += bias.unsqueeze(0).expand_as(output);
+        }
+
+        return output;
+    }
+
+    static tensor_list backward(AutogradContext *ctx, tensor_list grad_outputs) {
+        auto saved = ctx->get_saved_variables();
+        auto input = saved[0];
+        auto weight = saved[1];
+        auto bias = saved[2];
+
+        auto grad_output = grad_outputs[0];
+        auto grad_input = grad_output.mm(weight);
+        auto grad_weight = grad_output.t().mm(input);
+        auto grad_bias = torch::Tensor();
+
+        // Calculate the gradient bias, If it's defined
+        if (bias.defined()) {
+            grad_bias = grad_output.sum(0);
+        }
+
+        return {grad_input, grad_weight, grad_bias};
+    }
+};
+
+class MulConstant : public Function<MulConstant> {
+    public:
+        static torch::Tensor forward(AutogradContext *ctx, torch::Tensor tensor, double constant) {
+
+            // ctx is a context object that can be used to stash information
+            // for backward computation
+            ctx->saved_data["constant"] = constant;
+            return tensor * constant;
+        }
+
+        static tensor_list backward(AutogradContext *ctx, tensor_list grad_outputs) {
+            // We return as many input gradients as there were arguments.
+            // Gradients of non-tensor arguments to forward must be `torch::Tensor()`.
+            return {grad_outputs[0] * ctx->saved_data["constant"].toDouble(), torch::Tensor()};
+        }
+};
+
+void custom_autograd_function() {
+    std::cout << "====== Using Custom Autograd function in C++ ======" << std::endl;
+
+    {
+        auto x = torch::randn({2, 3}).requires_grad_();
+        auto weight = torch::randn({4, 3}).requires_grad_();
+        auto y = LinearFunction::apply(x, weight);
+        y.sum().backward();
+
+        std::cout << x.grad() << std::endl;
+        std::cout << weight.grad() << std::endl;
+    }
+
+    {
+        auto x = torch::randn({2}).requires_grad_();
+        auto y = MulConstant::apply(x, 5.5);
+        y.sum().backward();
+
+        std::cout << x.grad() << std::endl;
+    }
+}
+
 int main() {
     std::cout << std::boolalpha;
 
@@ -113,4 +188,7 @@ int main() {
     // Computation of the gradients
     compute_higher_order_gradients();
     std::cout << std::endl;
+
+    // Custom autograd
+    custom_autograd_function();
 }
