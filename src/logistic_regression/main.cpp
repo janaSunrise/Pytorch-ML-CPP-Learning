0f669e32247afa36ff94f589857ac362120c9d1a
janaSunrise <warriordefenderz@gmail.com>
2021-06-28 10:26:56 +0530

    Add code for datasets, and Device type [CPU, GPU].

--- src/logistic_regression/main.cpp
+++ src/logistic_regression/main.cpp
@@ -14,5 +14,37 @@ const double learning_rate = 0.001;
 const std::string data_path = "../../../data/mnist/";
 
 int main() {
-    std::cout << "Hello, world!";
+    // Set the seed
+    torch::manual_seed(1);
+
+    // Get the type of device
+    torch::DeviceType device_type;
+    if (torch::cuda::is_available()) {
+        std::cout << "CUDA available! Training on GPU." << std::endl;
+        device_type = torch::kCUDA;
+    } else {
+        std::cout << "Training on CPU." << std::endl;
+        device_type = torch::kCPU;
+    }
+
+    torch::Device device(device_type);
+
+    // Datasets
+    auto train_dataset = torch::data::datasets::MNIST(data_path)
+                         .map(torch::data::transforms::Normalize<>(0.1307, 0.3081))
+                         .map(torch::data::transforms::Stack<>());
+
+    const size_t train_dataset_size = train_dataset.size().value();
+
+    auto train_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
+        std::move(train_dataset), batch_size
+    );
+
+    auto test_dataset = torch::data::datasets::MNIST(data_path, torch::data::datasets::MNIST::Mode::kTest)
+                        .map(torch::data::transforms::Normalize<>(0.1307, 0.3081))
+                        .map(torch::data::transforms::Stack<>());
+
+    const size_t test_dataset_size = test_dataset.size().value();
+
+    auto test_loader = torch::data::make_data_loader(std::move(test_dataset), batch_size);
 }
