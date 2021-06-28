6ac38f6fc262859b52bb4ad9996b685ce95e50f6
janaSunrise <warriordefenderz@gmail.com>
2021-06-28 10:26:02 +0530

    Initialize the hyperparameters.

--- src/logistic_regression/main.cpp
+++ src/logistic_regression/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+// Hyperparameters
+const int64_t input_size = 784;
+
+const int64_t num_classes = 10;
+
+const int64_t batch_size = 100;
+
+const size_t num_epochs = 5;
+
+const double learning_rate = 0.001;
+
+const std::string data_path = "../../../data/mnist/";
+
 int main() {
     std::cout << "Hello, world!";
 }
